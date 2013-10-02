f90396fbcd5934cd46eef6de41bddbfd1a813074
IvanGromskyy <igromskyy@gmail.com>
2013-10-03 00:27:08 +0300

    Revert "Test first commit"
    This reverts commit e037d5ae759169450d21261ac4b917cfc34d2417.

--- TestForGitProject/src/test/TestMailHW.java
+++ TestForGitProject/src/test/TestMailHW.java
@@ -1,17 +1,10 @@
 package test;
 
-import java.util.Date;
-
 public class TestMailHW {
 
 	public static void main(String[] args) {
 		System.out.println("HI GIT");
-		System.out.println(getTime());
 
-	}
-	
-	public static String getTime() {
-		return "Time>>>" + new Date().getTime();
 	}
 
 }
