ae61a4a7a8dda620d03299e4a9435c6b6258a578
IvanGromskyy <igromskyy@gmail.com>
2013-10-07 22:41:32 +0300

    ig 2

--- TestForGitProject/src/test/TestMailHW.java
+++ TestForGitProject/src/test/TestMailHW.java
@@ -17,5 +17,9 @@ public class TestMailHW {
 	public static String getTime1() {
 		return "Time>>>" + new Date().getTime() + "L1";
 	}
+	
+	public static String getTime2() {
+		return "Time>>>" + new Date().getTime() + "L1";
+	}
 
 }
